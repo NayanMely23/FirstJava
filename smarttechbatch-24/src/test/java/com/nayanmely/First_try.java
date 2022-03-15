b388b69fe1d4cddd68c0f754554c62ce610fca83
nayan <nayan@DESKTOP-64KBUV2.myfiosgateway.com>
2022-03-15 18:30:57 -0400

    first test

---
+++ smarttechbatch-24/src/test/java/com/nayanmely/First_try.java
@@ -0,0 +1,10 @@
+package com.nayanmely;
+
+public class First_try {
+public static void main(String[] args) {
+System.out.println("hello java");
+System.out.println("hi selenium");
+}
+	}
+
+
---
+++ smarttechbatch-24/src/test/java/com/nayanmely/trial.java
@@ -0,0 +1,7 @@
+package com.nayanmely;
+
+public class trial {
+public static void main(String[] args) {
+	System.out.println("hello java");
+}
+}
